4ac8c6116ac5c25431952b2ee0487b9119d39910
a2adib <a3dib.life@gmail.com>
2025-12-05 22:20:35 +0600

    Adding UpdateJobs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -27,50 +27,57 @@ async function run() {
     await client.connect();
 
     const database = client.db('freelanceMarketplace');
-    const postJobs = database.collection('jobs');
-// post or save services from database
-    app.post('/jobs', async(req,res)=>{
-        const data = req.body;
-        const date = new Date();
-        data.CreatedAt = date;
-        console.log(data);
-        const result = await postJobs.insertOne(data);
-        res.send(result);
+    const freelanceMarketplace = database.collection('jobs');
+    // post or save services from database
+    app.post('/jobs', async (req, res) => {
+      const data = req.body;
+      const date = new Date();
+      data.CreatedAt = date;
+      console.log(data);
+      const result = await freelanceMarketplace.insertOne(data);
+      res.send(result);
     })
 
-// get or read services from database
-    app.get('/jobs', async(req,res)=>{
-        const result = await postJobs.find().toArray();
-        res.send(result);
+    // get or read services from database
+    app.get('/jobs', async (req, res) => {
+      const result = await freelanceMarketplace.find().toArray();
+      res.send(result);
     })
 
 
-// get single service using id
-    app.get('/jobs/:id', async(req,res)=>{
-        const id = req.params
-        console.log(id);
-        
-        const query = {_id: new ObjectId(id)};
-        const result = await postJobs.findOne(query);
-        res.send(result);
+    // get single service using id
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params
+      console.log(id);
+
+      const query = { _id: new ObjectId(id) };
+      const result = await freelanceMarketplace.findOne(query);
+      res.send(result);
     })
-// get jobs added by specific user
-  app.get('/myAddedJobs', async(req,res)=>{
-    const {email} = req.query
-    console.log(email);
-    const query = {userEmail: email};
-    const result = await postJobs.find(query).toArray();
-    res.send(result);
-  })
+    // get jobs added by specific user
+    app.get('/myAddedJobs', async (req, res) => {
+      const { email } = req.query
+      console.log(email);
+      const query = { userEmail: email };
+      const result = await freelanceMarketplace.find(query).toArray();
+      res.send(result);
+    })
 
-  app.put('/update/:id', async(req,res)=>{
-    const data = req.body;
-    const id = req.params
-    const query = {_id: new ObjectId(id)};
+    app.put('/update/:id', async (req, res) => {
+      const data = req.body;
+      delete data._id;
+      console.log(data)
+      const { id } = req.params
+      const query = { _id: new ObjectId(id) };
 
-    const updateJobs = {
-      $set: data
-  }})
+      const updateJobs = {
+        $set: data
+      }
+
+      const result = await freelanceMarketplace.updateOne(query, updateJobs);
+      res.send(result);
+      console.error("Update error:", err);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -80,13 +87,13 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send('hello developers')
+app.get('/', (req, res) => {
+  res.send('hello developers')
 })
 
 
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`);
-    
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+
 })
